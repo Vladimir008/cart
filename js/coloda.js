6dc88cefe0f1ca27b121845d135eab4c955701d6
Vladimir <vladimir.ship.virt@yandex.ru>
2018-01-29 21:54:00 +0300

    Заполнение первой строки арены
    Исправлены ошибки в создании узла

--- js/coloda.js
+++ js/coloda.js
@@ -1,4 +1,4 @@
-// Карта
+ /* --------------------------------------- КАРТА -----------------------------------------------*/
 function cart(obj){
 	if (obj == undefined){
 		obj = {};
@@ -23,22 +23,27 @@ function cart(obj){
 	}
 	this.strong = function(value){
 		if (!arguments.length) return strong;
-		if (value < 1) {value = 1} // Макс жизнь не может быть меньше нуля
+		if (value < 1) {value = 1} // Макс сила не может быть меньше 1
 		strong = value;
 	}
 	this.defense = function(value){	
 		if (!arguments.length) return defense;
-		if (value < 1) {value = 1} // Макс жизнь не может быть меньше нуля
+		if (value < 0) {value = 0} // Макс жизнь не может быть меньше нуля
 		defense = value;
 	}
-	this.max_live((obj.live!=undefined) ? obj.live : 10);
+	this.max_live((obj.live!=undefined) ? obj.live : 3);
 	this.live((obj.live!=undefined) ? obj.live : 1);
 	this.strong((obj.strong!=undefined) ? obj.strong : 1);
 	this.defense((obj.defense!=undefined) ? obj.defense : 0);
 
 	/* Построение html */ 
 	this.getInnerHtml = function(){
-		 var innerHtml = '<div class="cart-front"></div>'
+		 var innerHtml = '<div class="cart-front">'
+							+  this.name + '<br/>'
+							+  this.live() + '<br/>'
+							+  this.strong() + '<br/>'
+							+  this.defense() + '<br/>'
+		 				+ '</div>'
 		 				+ '<div class="cart-back"></div>';
 		 return innerHtml;
 	 }
@@ -58,39 +63,41 @@ function cart(obj){
 	 
 };
 
-//Колода карт
+ /* --------------------------------------- КОЛОДА КАРТ -----------------------------------------------*/
 function koloda(){
-
 	this.init = function(){
 		this.carts = [
-			new cart({name:'Карта1'}),
-			new cart({live:3}),
-			new cart({name:'Дама',strong:12}),
-			new cart({name:'Король',live:12}),
-			new cart({name:'Дама2',strong:12}),
-			new cart({name:'Дама3',strong:12}),
-			new cart({name:'Дама4',strong:12}),
-			new cart({name:'Дама5',strong:12}),
-			new cart({name:'Дама6',strong:12}),
-			new cart({name:'Дама7',strong:12}),
+			new cart({name:'Воин Бату',strong:2,live:2}),
+			new cart({name:'Воин Бату',strong:2,live:2}),
+			new cart({name:'Загонщик Бату',strong:1,live:3}),
+			new cart({name:'Загонщик Бату',strong:1,live:3}),
+			new cart({name:'Охотник Бату',strong:2,live:1}),
+			new cart({name:'Охотник Бату',strong:2,live:1}),
+			new cart({name:'Травник Бату',strong:1,live:1}),
+			new cart({name:'Травник Бату',strong:1,live:1}),
 		   ];
 	} 
 	
-	 this.getCart = function (){
+	 this.getCarts = function (count){
+		 if (count==undefined || isNaN(count) || count<=0) count = 0; 
 	 	 var carts = this.carts;
-	 	 var rand = Math.floor(Math.random() * carts.length);
-    	return carts.splice(rand,1)[0];
+		 var return_carts = []; 
+		 for (var i = 1; i<=count; i++){
+		 	var rand = Math.floor(Math.random() * carts.length);
+		  	return_carts.push(carts.splice(rand,1)[0]);
+		 }
+		  return return_carts;
 	}
 	return this.init();
 
 }
 
-//ячейка области
+ /* --------------------------------------- ЯЧЕЙКА -----------------------------------------------*/
  function cellCart(col,row,cart_obj){
  	this.row = row;
  	this.col = col;
  	this.cart = false;
-	this.cart = new cart({name:'Дама',strong:12}); // Временно
+	//this.cart = new cart({name:'Дама',strong:12}); // Временно
 	this.node = false;
 	this.setCart = function(cart_obj){
 		this.cart = cart_obj;
@@ -103,7 +110,8 @@ function koloda(){
 	}
 
 	this.setNode = function(){
-	   var cartCellNode = document.createElement('div');
+	  
+	   var cartCellNode = (!this.node) ? document.createElement('div') : this.node;
 	   cartCellNode.className = "cell-cart";
 	   cartCellNode.setAttribute("row",this.row);
 	   cartCellNode.setAttribute("col",this.col);
@@ -111,6 +119,7 @@ function koloda(){
 			cartCellNode.appendChild(this.cart.getNode());
 	   }
 	   this.node = cartCellNode;
+	 	console.log(this.node);  
 	   return this.node;
 	}
 	this.getNode = function(){
@@ -121,7 +130,7 @@ function koloda(){
 	}
  };
 
- // --------Группа ячеек
+ /* --------------------------------------- ГРУППА ЯЧЕЕК -----------------------------------------------*/
  function blockCells(col,row,inverse){
 	 
 	 this.col = (col!=undefined && col > 0) ? col : 1;
@@ -129,19 +138,13 @@ function koloda(){
 	 this.inverse = (inverse!=undefined) ? inverse : false;
 	 this.cells = [];
 	 this.node = false;
-	 this.init = function(){
-		 if (this.inverse){ // Ячейки в обратном порядке 
-			for (var j = this.row; j>=1; j--){
-				for (var i = 1; i<=this.col; i++){
-					this.cells.push(new cellCart(i,j));
-				}
-			}
-		 }
-		 else{
-			for (var j = 1; j<=this.row; j++){
-				for (var i = 1; i<=this.col; i++){
-					this.cells.push(new cellCart(i,j));
-				}
+	 this.init = function(){	
+		for (var j = (this.inverse) ? this.col : 1 ; 
+		(this.inverse) ? (j>=1) : (j<=this.row); 
+		(this.inverse) ? j-- : j++
+		){
+			for (var i = 1; i<=this.col; i++){
+				this.cells.push(new cellCart(i,j));
 			}
 		}
 		 return true;
@@ -150,19 +153,20 @@ function koloda(){
 	 this.setNode = function(){
 		var blockCellsNode = document.createElement('div');
 		blockCellsNode.className = "block-cells";
-		var blockCellsRowNode = document.createElement('div');
-		blockCellsRowNode.className = 'block-cells__row';
-		var selectRow = (this.inverse) ? this.row : 1;
- 		this.cells.forEach(function(item, i){
-			if (item.row!=selectRow){
-				blockCellsNode.appendChild(blockCellsRowNode.cloneNode(true));
-				blockCellsRowNode = document.createElement('div');
-				blockCellsRowNode.className = 'block-cells__row';
-				selectRow = item.row; 
-			}
-			blockCellsRowNode.appendChild(item.getNode());
-		});
-		blockCellsNode.appendChild(blockCellsRowNode);
+		// Идем построчно
+		for (var i = (this.inverse) ? this.row : 1 ; 
+			(this.inverse) ? (i>=1) : (i<=this.row); 
+			(this.inverse) ? i-- : i++
+		){
+			var blockCellsRowNode = document.createElement('div');
+			blockCellsRowNode.className = 'block-cells__row';
+			this.cells.forEach(function(item){
+				if (item.row==i){ // Выбираем только те ячейки которык должны быть в этой строке
+					blockCellsRowNode.appendChild(item.getNode());
+				}
+			});
+			blockCellsNode.appendChild(blockCellsRowNode);
+		}
 		this.node = blockCellsNode;
 		return this.node;
 	 }
@@ -172,9 +176,17 @@ function koloda(){
 		 }
 		 return this.node;
 	 }
+	 this.getCell = function(col,row){ 
+		var num = (!this.inverse) ? this.row*(row-1)+(col-1) : this.row*(this.row - row)+(col-1); 
+		if (this.cells[num]==undefined){
+			console.error("Нет такой ячейки");
+		}
+		return this.cells[num];
+	 };
 	 return this.init();
  }
 
+ /*--------------- КАРТА ИГРОКА ------------------------------------*/
  function playerProfile(obj){
 	if (obj == undefined){
 		obj = {};
@@ -182,12 +194,15 @@ function koloda(){
 	this.node = false;
 	this.init = function(){
 		 this.name = "Игрок";
-		 this.cart = new cart({name:"Игрок"});
+		 this.cellCart = new cellCart(1,1,new cart({name:"Игрок"}));
+		 this.nps = (obj.nps!=undefined) ? obj.nps : true;
+		 this.opponent = (obj.opponent!=undefined) ? obj.opponent : false;
 	}
 	this.setNode = function(){
 		var playerNode = document.createElement('div');
 		playerNode.className = "player";
 		playerNode.innerHTML = '';
+		playerNode.appendChild(this.cellCart.getNode());
 		this.node = playerNode;
 		return this.node;
 	 }
@@ -199,7 +214,7 @@ function koloda(){
 	 }
 	 return this.init();
 }	 
- // Игровое место (половина стола)
+ /*--------------- ИГРОВАЯ ЗОНА ИГРОКА (его половина стола) ------------------------------------*/
  function halfTable(obj){
 	if (obj == undefined){
 		obj = {};
@@ -217,6 +232,7 @@ function koloda(){
  		this.spells = new blockCells(this.spells_col);
 		this.items = new blockCells(this.items_col);
 		this.player = new playerProfile();
+		this.koloda = new koloda();
  	}
 
 	 this.setNode = function(){
@@ -248,7 +264,7 @@ function koloda(){
 			+ '<div class="half-table__right">'
 			+ '</div>';
 		}
-		console.log(halfTableNode.querySelector(".half-table__items"));
+
 		halfTableNode.querySelector(".half-table__items").appendChild(this.items.getNode());
 		halfTableNode.querySelector(".half-table__spells").appendChild(this.spells.getNode());
 		halfTableNode.querySelector(".half-table__arena").appendChild(this.arena.getNode());
@@ -262,8 +278,27 @@ function koloda(){
 		 }
 		 return this.node;
 	 }
+	 // Получить карты из колоды
+	 this.getCartsFromColoda = function(count){
+		if (count==undefined || isNaN(count) || count<=0) count = 0; 
+		return this.koloda.getCarts(count);
+	 }
+	 // Заполним первую строку картами
+	 this.fillFirstRow = function(){
+		var carts = this.getCartsFromColoda(this.arena_col);
+		for(var i = 1; i<=this.arena_col;i++){
+			var selectCell = this.arena.getCell(i,1);
+				selectCell.setCart(carts.shift());
+				selectCell.setNode();
+		}
+		// Продолдим потом ....
+	 }
+	 
+
  	return this.init();
  }
+
+  /*-------------------------- ИГРОВОЙ СТОЛ ------------------------------------*/
  function Table(obj){
 	 	if (obj == undefined){
 			obj = {};
@@ -300,6 +335,3 @@ function koloda(){
 		}
 	 	return this.init();
  }
-
- 
- //console.log(koloda.getCart());
